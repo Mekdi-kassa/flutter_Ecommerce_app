9b1b0bc01352c87105b0127de5df29e246fafaab
Medkelawit_Abadina <mekdelawitkassa6@gmail.com>
2025-08-05 12:36:59 +0300

    task14

--- lib/core/datasource/product_local_datasource.dart
+++ lib/core/datasource/product_local_datasource.dart
@@ -1,13 +1,24 @@
-class ProductLocalDataSource {
-  final List<dynamic> _localProducts = [];
+import 'package:shared_preferences/shared_preferences.dart';
 
-  Future<List<dynamic>> getProducts() async {
-    // Simulate local fetch
-    return _localProducts;
+abstract class ProductLocalDataSource {
+  Future<List<String>> getProducts();
+  Future<void> addProduct(String product);
+}
+
+class ProductLocalDataSourceImpl implements ProductLocalDataSource {
+  static const String _productsKey = 'products';
+
+  @override
+  Future<List<String>> getProducts() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(_productsKey) ?? [];
   }
 
-  Future<void> addProduct(dynamic product) async {
-    _localProducts.add(product);
+  @override
+  Future<void> addProduct(String product) async {
+    final prefs = await SharedPreferences.getInstance();
+    final products = prefs.getStringList(_productsKey) ?? [];
+    products.add(product);
+    await prefs.setStringList(_productsKey, products);
   }
-  
 }
