d32004a30b89f0ce29c38e2cfdb458ec8302a161
Medkelawit_Abadina <mekdelawitkassa6@gmail.com>
2025-08-14 12:21:23 +0300

    Task13

--- lib/core/plateform/network_info.dart
+++ lib/core/plateform/network_info.dart
@@ -1,4 +1,14 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
 }
 // for the network to check if it's connected or not pluu many use when we fetch from the api
+class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker connectionChecker;
+
+  NetworkInfoImpl(this.connectionChecker);
+
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
+}
